fa3601c2f683530fcb3424f8eccd06e0a415c6c0
Jacinta <162335518+jhelm1217@users.noreply.github.com>
2024-06-25 14:19:17 -0400

    edit my imports for my images

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,137 +1,7 @@
-// import { Link } from "react-router-dom"
-// import './App.css'
-// // import bootcamp from "assets/bootcamp.jpg"
-
-
-
-// function Contact () {
-//   return (
-    
-//     <div className="contact-background"> 
-//     <div className="header-container">
-
-//     <Link to='/'>{'<- Back'} </Link>
-//     </div>
-//     <div className="p-5">
-//       <h1 style={{ color: "Black" }}>Reach Out to Me! </h1>
-//       <div className="contact-card-wrapper">
-// 	    <div className="row">
-// 		    <div className="container col-12 col-sm-6 p-2">
-// 		        <img src="assets/bootcamp.jpg" class="card-img-top hover contact-img" alt="My Picture" />
-//                 <div className="card-body">
-//                     <div className="border"> 
-//                         <h1 className="card-title text-center">Jacinta Helm</h1>
-//                         <div className="social-icons">
-//                           <a href="https:jacintahelm92@gmail.com" target="_blank" rel="noopener noreferrer">
-//                             <img src="https://img.icons8.com/color/48/000000/gmail-new.png" alt="Gmail" />
-//                           </a>
-//                           <a href="https://github.com/jhelm1217" target="_blank" rel="noopener noreferrer">
-//                             <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" alt="GitHub" />
-//                           </a>
-//                           <a href="https://www.linkedin.com/in/jacintah12/" target="_blank" rel="noopener noreferrer">
-//                             <img src="https://content.linkedin.com/content/dam/me/brand/en-us/brand-home/logos/In-Blue-Logo.png" alt="LinkedIn" />
-//                           </a>
-//                         </div>
-                    
-                       
-//                     </div>
-                       
-//                 </div>
-// 	        </div>
-//         </div>
-//       </div>
-//     </div>
-//     </div>
-
-//   )
-// }
-
-
-
-// export default Contact
-
-
-// import { Link } from "react-router-dom";
-// import './App.css';
-// // import bootcamp from "assets/bootcamp.jpg";
-
-// function Contact() {
-//   return (
-//     <div className="contact-background">
-//       <div className="header-container">
-//         <Link to='/'>{'<- Back'}</Link>
-//       </div>
-//       <div className="p-5">
-//         <h1 style={{ color: "Black" }}>Reach Out to Me!</h1>
-//         <div className="contact-card-wrapper">
-//           <div className="row">
-//             <div className="container col-12 col-sm-6 p-2">
-//               <img
-//                 src="assets/bootcamp.jpg"
-//                 className="card-img-top hover contact-img"
-//                 alt="My Picture"
-//               />
-//               <div className="card-body">
-//                 {/* <div className="border"> */}
-//                 <div className="contact-card">
-//             <div className="profile-section">
-//                 <img src="src/assets/bootcamp.jpg" alt="Profile" className="profile-pic" />
-//                 <div className="profile-info">
-//                     <h2 className="name">Jacinta Helm</h2>
-//                     <p className="title">Full Stack Web Developer</p>
-//                 </div>
-//             </div>
-//                   {/* <h1 className="card-title text-center">Jacinta Helm</h1> */}
-//                   <div className="social-icons">
-//                     <a
-//                       href="https://mail.google.com/mail/u/0/#inbox"
-//                       target="_blank"
-//                       rel="noopener noreferrer"
-//                     >
-//                       <img
-//                         src="https://img.icons8.com/color/48/000000/gmail-new.png"
-//                         alt="Gmail"
-//                         className="icon"
-//                       />
-//                     </a>
-//                     <a
-//                       href="https://github.com/jhelm1217"
-//                       target="_blank"
-//                       rel="noopener noreferrer"
-//                     >
-//                       <img
-//                         src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-//                         alt="GitHub"
-//                         className="icon"
-//                       />
-//                     </a>
-//                     <a
-//                       href="https://www.linkedin.com/in/jacintah12/"
-//                       target="_blank"
-//                       rel="noopener noreferrer"
-//                     >
-//                       <img
-//                         src="https://content.linkedin.com/content/dam/me/brand/en-us/brand-home/logos/In-Blue-Logo.png"
-//                         alt="LinkedIn"
-//                         className="icon"
-//                       />
-//                     </a>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Contact;
-
 import { Link } from "react-router-dom";
 import './contact.css';
 // import bootcamp from "assets/bootcamp.jpg";
+import bootcamp from "./assets/bootcamp.jpg"
 
 function Contact() {
   return (
@@ -148,7 +18,8 @@ function Contact() {
                 <div className="card-body">
                   <div className="contact-card">
                     <div className="profile-section">
-                      <img src="src/assets/bootcamp.jpg" alt="Profile" className="profile-pic" />
+                      <img src={bootcamp} alt="Profile" className="profile-pic" />
+                      
                       <div className="profile-info">
                         <h2 className="name">Jacinta Helm</h2>
                       </div>
@@ -199,4 +70,4 @@ function Contact() {
   );
 }
 
-export default Contact;
+export default Contact
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import './App.css'; // Make sure you have styles for the project cards in your CSS file
 // import './projects.css'
+import final from "./assets/packyourbags.png"
+import tic from "./assets/tictactoe.png"
+import weather from "./assets/weather.png"
+import list from "./assets/todolist.png"
 
 function Projects() {
   return (
@@ -13,7 +17,7 @@ function Projects() {
         <div className="project-card">
           <h2 style={{ color: 'white' }}>Pack Your Bags!</h2>
           <a href="https://pack-bags.vercel.app/" target="_blank" rel="noopener noreferrer">
-            <img src="src/assets/packyourbags.png" alt="Pack Your Bags!" className="project-screenshot" />
+            <img src={final} alt="Pack Your Bags!" className="project-screenshot" />
             <p style={{ color: 'white' }}> This is my final project that I presented for my capstone project for my demo day. </p>
           </a>
 
@@ -21,7 +25,7 @@ function Projects() {
         <div className="project-card">
           <h2 style={{ color: 'white' }}>My Tic Tac Toe Game!</h2>
           <a href="https://jhelm1217.github.io/tic-tac-toe/" target="_blank" rel="noopener noreferrer">
-            <img src="/src/assets/tictactoe.png" alt="Tic Tac Toe Game" className="project-screenshot" />
+            <img src={tic} alt="Tic Tac Toe Game" className="project-screenshot" />
           </a>
           <p style={{ color: 'white' }}>I made this game in week 3 of my BootCamp.</p>
         </div>
@@ -29,14 +33,14 @@ function Projects() {
         <div className="project-card">
           <h2 style={{ color: 'white' }}>The Weather App</h2>
           <a href="https://jhelm1217.github.io/weather-app/" target="_blank" rel="noopener noreferrer">
-            <img src="/src/assets/weather.png" alt="Weather App" className="project-screenshot" />
+            <img src={weather} alt="Weather App" className="project-screenshot" />
           </a>
           <p style={{ color: 'white' }}>This weather app allows you to input the name of a city and get the current temperature.</p>
         </div>
         <div className="project-card">
           <h2 style={{ color: 'white' }}>To Do List!</h2>
           <a href="https://todo-list-weld-kappa.vercel.app/" target="_blank" rel="noopener noreferrer">
-            <img src="/src/assets/todolist.png" alt="To Do List" className="project-screenshot" />
+            <img src={list} alt="To Do List" className="project-screenshot" />
           </a>
           <p style={{ color: 'white' }}>A fun to do list for you to keep track of all your tasks!</p>
         </div>
